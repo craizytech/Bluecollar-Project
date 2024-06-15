de4fa03c3e558f740aaf6a314508b1db728d8318
Lennox Kabo <borelennox@gmail.com>
2024-06-15 20:38:05 +0300

    header section and hero from lexx

---
+++ frontend/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Hero from "./_components/Hero";
+import CategoryList from "./_components/CategoryList";
+
+export default function Home() {
+  return (
+    <div>
+       <Hero/>
+       <CategoryList/>
+    </div>
+  );
+}
+
