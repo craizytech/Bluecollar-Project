d8de43e8c5dfea0c771b6a48608e35e4e5690fda
Lennox Kabo <borelennox@gmail.com>
2024-06-17 10:09:38 +0300

    popular business frontend completion

--- frontend/app/_components/CategoryList.js
+++ frontend/app/_components/CategoryList.js
@@ -1,9 +1,40 @@
+import Image from 'next/image'
 import React from 'react'
 
 function CategoryList() {
-  return (
-    <div>CategoryList</div>
-  )
+    return (
+        <div className="mt-14 mx-4 md:mx-22 lg:mx-52 grid grid-cols md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="flex flex-col items-center justify-center gap-2 bg-blue-50 p-4 rounded-lg hover:cursor-pointer hover:scale-110 transition-all ease-in-out">
+                <Image src="/mop.png"
+                    alt="icon"
+                    width={35}
+                    height={35} />
+
+                <h2 className="text-primary">Cleaning</h2>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2 bg-blue-50 p-4 rounded-lg hover:cursor-pointer hover:scale-110 transition-all ease-in-out">
+                <Image src="/bricklayer.png"
+                    alt="icon"
+                    width={35}
+                    height={35} />
+                <h2 className="text-primary">Masonry</h2>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2 bg-blue-50 p-4 rounded-lg hover:cursor-pointer hover:scale-110 transition-all ease-in-out">
+                <Image src="/repairing.png"
+                    alt="icon"
+                    width={35}
+                    height={35} />
+                <h2 className="text-primary">Plumbing</h2>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2 bg-blue-50 p-4 rounded-lg hover:cursor-pointer hover:scale-110 transition-all ease-in-out">
+                <Image src="/energy.png"
+                    alt="icon"
+                    width={35}
+                    height={35} />
+                <h2 className="text-primary">Electric</h2>
+            </div>
+        </div>
+    )
 }
 
 export default CategoryList
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Hero from "./_components/Hero";
 import CategoryList from "./_components/CategoryList";
+import BusinessList from "./_components/BusinessList";
 
 export default function Home() {
   return (
     <div>
        <Hero/>
        <CategoryList/>
+       <BusinessList title={'Popular Business'}/>
     </div>
   );
 }
