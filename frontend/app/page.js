5f686da2e07e657d4f1a07b3a0e78aa9bfee28f1
Njeriwaweru <njeripraise318@gmail.com>
2024-06-17 13:35:30 +0300

    UserProfile page created

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import UserProfile from "./_components/UseProfile";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
