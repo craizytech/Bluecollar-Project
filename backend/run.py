0eda9a963ab697d79b6635720a56ebae4ed195a6
Lennox Kabo <borelennox@gmail.com>
2024-08-06 12:30:09 +0300

    admin side

--- backend/run.py
+++ backend/run.py
@@ -2,6 +2,6 @@ from app import create_app
 from app.auth import auth_bp
 
 app = create_app()
-
+app.config['UPLOAD_FOLDER'] = '../frontend/public'
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
