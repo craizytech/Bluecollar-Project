ff1cb2b2ef8f6f3aff11b81f5cd7c658a571cf06
craizytech <craizytech@gmail.com>
2024-08-08 16:24:29 +0300

    removed some bloatware

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -8,14 +8,3 @@ db = SQLAlchemy()
 migrate = Migrate()
 cors = CORS()
 jwt = JWTManager()
-
-# Initialize JWT with the app
-def init_jwt(app):
-    jwt.init_app(app)
-
-@jwt.token_in_blocklist_loader
-def check_if_token_in_blocklist(jwt_header, jwt_payload):
-    from app.models import TokenBlockList
-    jti = jwt_payload['jti']
-    token = TokenBlockList.query.filter_by(jti=jti).first()
-    return token is not None
